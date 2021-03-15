c522b5d1999d5b880b885a51f8f253012ea94294
Balázs Orbán <info@balazsorban.com>
2021-03-15 23:49:29 +0100

    chore: add provider generation error logging

--- www/scripts/generate-providers.js
+++ www/scripts/generate-providers.js
@@ -7,9 +7,14 @@ const files = fs.readdirSync(providersPath, 'utf8')
 
 const result = files.reduce((acc, file) => {
   const provider = fs.readFileSync(path.join(providersPath, file), 'utf8')
-  const { id, title } = provider.match(/id: (?<id>.+)\ntitle: (?<title>.+)\n/).groups
-  acc[id] = title
-  return acc
+  try {
+    const { id, title } = provider.match(/id: (?<id>.+)\ntitle: (?<title>.+)\n/).groups
+    acc[id] = title
+    return acc
+  } catch (error) {
+    console.error('Could not read provider header', { file: provider, fileName: file })
+    throw error
+  }
 }, {})
 
 fs.writeFileSync(path.join(process.cwd(), 'providers.json'), JSON.stringify(result, null, 2))
